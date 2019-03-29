f8cd941318d2b39cf941bbdfc56b06b416d0aa30
Petr Fedorov <petr.fedorov@phystech.edu>
2019-03-29 10:49:32 +0300

    order_book() function and appropriate processing of time zones in obAnalyticsDb package

--- R/obanalytics.R
+++ R/obanalytics.R
@@ -1,6 +1,8 @@
 #' @export
 depth <- function(conn, start.time, end.time, exchange, pair, debug.query = FALSE) {
-
+  tzone <- attr(end.time, "tzone")
+  start.time <- format(start.time, usetz=T)
+  end.time <- format(end.time, usetz=T)
 
   query <- paste0(" SELECT obanalytics._in_milliseconds(timestamp) AS timestamp,
                   price, volume, side FROM obanalytics.oba_depth(",
@@ -13,13 +15,18 @@ depth <- function(conn, start.time, end.time, exchange, pair, debug.query = FALS
   depth <- DBI::dbGetQuery(conn, query)
   depth$timestamp <- as.POSIXct(as.numeric(depth$timestamp)/1000, origin="1970-01-01")
   depth$side <- factor(depth$side, c("bid", "ask"))
-  attr(depth$timestamp, 'tzone') <- ""
+  attr(depth$timestamp, 'tzone') <- tzone
   depth
 }
 
 
 #' @export
-spread <- function(conn, start.time, end.time, exchange, pair, only.different = TRUE, debug.query = FALSE) {
+spread <- function(conn, start.time, end.time, exchange, pair,  only.different = TRUE, debug.query = FALSE) {
+  tzone <- attr(end.time, "tzone")
+
+  start.time <- format(start.time, usetz=T)
+  end.time <- format(end.time, usetz=T)
+
   query <- paste0(" SELECT 	obanalytics._in_milliseconds(timestamp) AS timestamp,
                   \"best.bid.price\",
                   \"best.bid.volume\",
@@ -34,7 +41,7 @@ spread <- function(conn, start.time, end.time, exchange, pair, only.different = 
                   " ) ORDER BY 1")
   if(debug.query) cat(query)
   spread <- DBI::dbGetQuery(conn, query)
-  spread$timestamp <- as.POSIXct(as.numeric(spread$timestamp)/1000, origin="1970-01-01")
+  spread$timestamp <- as.POSIXct(as.numeric(spread$timestamp)/1000, origin="1970-01-01", tz=tzone)
   spread
 }
 
@@ -42,6 +49,11 @@ spread <- function(conn, start.time, end.time, exchange, pair, only.different = 
 
 #' @export
 events <- function(conn, start.time, end.time, exchange, pair, debug.query = FALSE) {
+  tzone <- attr(end.time, "tzone")
+
+  start.time <- format(start.time, usetz=T)
+  end.time <- format(end.time, usetz=T)
+
   query <- paste0(" SELECT 	\"event.id\"::integer,
                   \"id\"::numeric,
                   obanalytics._in_milliseconds(timestamp) AS timestamp,
@@ -62,7 +74,7 @@ events <- function(conn, start.time, end.time, exchange, pair, debug.query = FAL
                   ")")
   if(debug.query) cat(query)
   events <- DBI::dbGetQuery(conn, query)
-  events$timestamp <- as.POSIXct(as.numeric(events$timestamp)/1000, origin="1970-01-01")
+  events$timestamp <- as.POSIXct(as.numeric(events$timestamp)/1000, origin="1970-01-01", tz=tzone)
   events$action <- factor(events$action, c("created", "changed", "deleted"))
   events$direction <- factor(events$direction, c("bid", "ask"))
   events$type <- factor(events$type, c("unknown", "flashed-limit",
@@ -72,6 +84,11 @@ events <- function(conn, start.time, end.time, exchange, pair, debug.query = FAL
 
 #' @export
 trades <- function(conn, start.time, end.time, exchange, pair, debug.query = FALSE) {
+  tzone <- attr(end.time, "tzone")
+
+  start.time <- format(start.time, usetz=T)
+  end.time <- format(end.time, usetz=T)
+
   query <- paste0(" SELECT 	obanalytics._in_milliseconds(timestamp) AS timestamp,
                   price, volume, direction,
                   \"maker.event.id\"::integer,
@@ -86,7 +103,7 @@ trades <- function(conn, start.time, end.time, exchange, pair, debug.query = FAL
                   ") ORDER BY timestamp")
   if(debug.query) cat(query)
   trades <- DBI::dbGetQuery(conn, query)
-  trades$timestamp <- as.POSIXct(as.numeric(trades$timestamp)/1000, origin="1970-01-01")
+  trades$timestamp <- as.POSIXct(as.numeric(trades$timestamp)/1000, origin="1970-01-01", tz=tzone)
   trades$direction <- factor(trades$direction, c("buy", "sell"))
 
   trades
@@ -95,6 +112,9 @@ trades <- function(conn, start.time, end.time, exchange, pair, debug.query = FAL
 
 #' @export
 depth_summary <- function(conn, start.time, end.time, exchange, pair, precision='r0', debug.query = FALSE) {
+  tzone <- attr(end.time, "tzone")
+  start.time <- format(start.time, usetz=T)
+  end.time <- format(end.time, usetz=T)
 
   query <- paste0("SELECT obanalytics._in_milliseconds(timestamp) AS timestamp,
                   side, bps_level, price, volume FROM obanalytics.oba_depth_summary(",
@@ -126,12 +146,49 @@ depth_summary <- function(conn, start.time, end.time, exchange, pair, precision=
   df[setdiff(bid.names, colnames(df))] <- 0
   df[setdiff(ask.names, colnames(df))] <- 0
   df[is.na(df)] <- 0
+  attr(df$timestamp, 'tzone') <- tzone
   df
 }
 
 
 #' @export
+order_book <- function(conn, tp, exchange, pair, max.levels = NA, bps.range = NA, min.bid = NA, max.ask = NA, debug.query = FALSE) {
+
+  tzone <- attr(tp, "tzone")
+  tp <- format(tp, usetz=T)
+  if (is.na(max.levels)) max.levels <- "NULL"
+  if (is.na(bps.range)) bps.range <- "NULL"
+  if (is.na(min.bid)) min.bid <- "NULL"
+  if (is.na(max.ask)) max.ask <- "NULL"
+
+
+  query <- paste0("select ts, \"timestamp\", id, price, volume, liquidity, bps, side, \"exchange.timestamp\"
+                   from obanalytics.oba_order_book(",
+                  shQuote(tp), ",",
+                  "obanalytics.oba_pair_id(",shQuote(pair),"), " ,
+                  "obanalytics.oba_exchange_id(", shQuote(exchange), "), ",
+                  max.levels,  ",",
+                  bps.range,  ",",
+                  min.bid,  ",",
+                  max.ask, ")")
+  if(debug.query) cat(query)
+  full_book <- DBI::dbGetQuery(conn, query)
+  cols <- c("id","timestamp", "exchange.timestamp", "price", "volume", "liquidity", "bps")
+  bids <- full_book[which(full_book$side == 'b'), cols ]
+  asks <- full_book[which(full_book$side == 's'), cols ]
+  ts <- full_book$ts[1]
+  attr(ts, "tzone") <- tzone
+  list(timestamp=ts, asks=asks, bids=bids)
+}
+
+
+
+
+#' @export
 export <- function(conn, start.time, end.time, exchange, pair, file = "events.csv", debug.query = FALSE) {
+  start.time <- format(start.time, usetz=T)
+  end.time <- format(end.time, usetz=T)
+
   query <- paste0(" select * from obanalytics.oba_export(", shQuote(start.time),
                   ", ",
                   shQuote(end.time), ", ",
