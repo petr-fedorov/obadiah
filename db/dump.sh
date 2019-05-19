bc6ba22f71d79aae18c4dfaa1e5b1b895a9b2960
Petr Fedorov <petr.fedorov@phystech.edu>
2019-05-19 12:31:20 +0300

    dump.sh now accepts an optional second parameter - an output directory for the dump

--- db/dump.sh
+++ db/dump.sh
@@ -1,6 +1,7 @@
-pg_dump -U ob-analytics -d $1 -n bitfinex -s -T bitfinex.*00000*  > bitfinex_schema.sql
-pg_dump -U ob-analytics -d $1 -n bitstamp -s  > bitstamp_schema.sql
-pg_dump -U ob-analytics -d $1 -t bitstamp.pairs -a > bitstamp_pairs.sql
-pg_dump -U ob-analytics -d $1 -n obanalytics -s > obanalytics_schema.sql
-pg_dump -U ob-analytics -d $1 -t obanalytics.pairs -a > obanalytics_pairs.sql
-pg_dump -U ob-analytics -d $1 -t obanalytics.exchanges -a > obanalytics_exchanges.sql
+
+pg_dump -U ob-analytics -d $1 -n bitfinex -s -T bitfinex.*00000*  > ${2:-.}/bitfinex_schema.sql
+pg_dump -U ob-analytics -d $1 -n bitstamp -s  > ${2:-.}/bitstamp_schema.sql
+pg_dump -U ob-analytics -d $1 -t bitstamp.pairs -a > ${2:-.}/bitstamp_pairs.sql
+pg_dump -U ob-analytics -d $1 -n obanalytics -s > ${2:-.}/obanalytics_schema.sql
+pg_dump -U ob-analytics -d $1 -t obanalytics.pairs -a > ${2:-.}/obanalytics_pairs.sql
+pg_dump -U ob-analytics -d $1 -t obanalytics.exchanges -a > ${2:-.}/obanalytics_exchanges.sql
